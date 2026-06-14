85e929868d791ccecb69f0f30a13855db917487e
Jax-Wysong <jgwysong@gmail.com>
2026-06-13 21:09:23 -0400

    updating main comments

--- MHD/2D-MHD/main.py
+++ MHD/2D-MHD/main.py
@@ -1,8 +1,77 @@
 '''
-2D Ideal MHD solver based off of Gardiner and Stone's 2005 paper
-which describes a constrained transport (CT) method for handling the
-divergence free constraint and the corner transport upwind method
-(CTU) for 2nd order accuracy.
+####################################################################################################
+2D Magnetohydrodynamic Equations
+Jax Wysong
+06/13/2026
+####################################################################################################
+
+The 2D Magnetohydrodynamic Equations are given by
+
+∂(ρ)/∂t     + ∇·(ρu)                                   = 0
+∂(ρu)/∂t    + ∇·(ρu⊗u + (p + 0.5*∥B∥^2 )I - B⊗B )     = 0
+∂(B)/∂t     + ∇·( u⊗B - B⊗u )                        = 0
+∂(E)/∂t     + ∇·( u(E + p + 0.5*∥B∥^2 ) - B(u·B))      = 0
+
+with the additional requirement that ∇·B = 0
+
+ρ is the density
+u is the velocity vector (u_x,u_y,u_z)
+B is the magnetic field vector (b_x,b_y,b_z)
+E is the total energy
+p is the  pressure
+I is the identity matrix
+E is the total energy = (p / (γ-1)) + 0.5ρ*∥u∥^2 + 0.5*∥B∥^2
+γ is the adiabatic index = 5/3 
+∥·∥ is the Euclidean vector norm
+
+Written slightly different we have
+
+∂q/∂t + ∂F(q)/∂x + ∂G(q)/∂y = 0
+
+where the conserved variables q are
+
+        | ρ    |
+        | ρu_x |
+        | ρu_y |
+q =     | ρu_z |
+        | E    |
+        | B_x  |
+        | B_y  |
+        | B_z  |
+
+        |              ρu_x                |
+        | ρu_xu_x + p + 0.5*∥B∥^2 - B_xB_x  |
+        |        ρu_xu_y - B_xB_y          |
+F(q) =  |        ρu_xu_z - B_xB_z          |
+        | u_x(E + p + 0.5*∥B∥^2) - B_x(u·B) |
+        |               0                  |
+        |        u_xB_y - u_yB_x           |
+        |        u_xB_z - u_zB_x           |
+
+        |              ρu_y                |
+        |        ρu_yu_x - B_yB_x          |
+        | ρu_yu_y + p + 0.5*∥B∥^2 - B_yB_y  |
+G(q) =  |        ρu_yu_z - B_yB_z          |
+        | u_y(E + p + 0.5*∥B∥^2) - B_y(u·B) |
+        |        u_yB_x - u_xB_y           |
+        |               0                  |
+        |        u_yB_z - u_zB_y           |
+
+For convenience, we also introduce the primative variables w
+
+        | ρ   |
+        | u_x |
+        | u_y |
+w =     | u_z |
+        | p   |
+        | B_x |
+        | B_y |
+        | B_z |
+
+I am solving this system of equations using a Finite Volume Unsplit, Corner Transport Upwind (CTU) method 
+with Constrained Transport (CT).
+The CTU procedure follows that described by Zingale and Gardiner and Stone (2005), GS05
+and the CT is described by GS05.
 '''
 
 
